4c402e3883cf0a59713ebbbdae9adb935ad5a8ef
Alex Busch <abusch1013@gmail.com>
2018-08-04 21:10:06 -0400

    Updates

--- components/Home.js
+++ components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { Header, ListItem, Icon } from 'react-native-elements';
+import LinearGradient from 'react-native-linear-gradient';
 
 export default class Home extends React.Component {
   static navigationOptions = {
@@ -16,42 +17,54 @@ export default class Home extends React.Component {
   render() {
     const { navigation } = this.props;
     return (
-      <View style={styles.container}>
+      <React.Fragment>
         <Image
           source={require('../greens.jpg')}
           style={styles.background}
           resizeMode="cover"
           opacity={.5}
         />
-        <Header
-          backgroundColor={'transparent'}
-          outerContainerStyles={{ borderBottomWidth: 0 }}
-          innerContainerStyles={{ margin: 5 }}
-          leftComponent={{ icon: 'menu', color: '#10ac84', size: 36, onPress: () => navigation.openDrawer() }}
-          // rightComponent={{ icon: 'home', color: '#10ac84', size: 36 }}
-          rightComponent={<Text style={{ color: '#10ac84', fontSize: 24 }}>{navigation.state.routeName}</Text>}
-        />
-        <View style={styles.content}>
-          <Text style={{ padding: 25 }}>Explore our menu to get started!</Text>
-          <ListItem
-            leftIcon={{ name: 'redo', color: '#78e08f' }}
-            style={{ backgroundColor: '#fefefe' }}
-            title="Login to get started!"
+        <View style={styles.container}>
+          <Header
+            backgroundColor={'transparent'}
+            outerContainerStyles={{ borderBottomWidth: 0 }}
+            innerContainerStyles={{ margin: 5 }}
+            leftComponent={{ icon: 'menu', color: '#10ac84', size: 36, onPress: () => navigation.openDrawer() }}
+            // rightComponent={{ icon: 'home', color: '#10ac84', size: 36 }}
+            rightComponent={<Text style={{ color: '#10ac84', fontSize: 24 }}>{navigation.state.routeName}</Text>}
           />
-          <ListItem
-            leftIcon={{ name: 'restaurant-menu', color: '#78e08f' }}
-            title="Check out the menu!"
+          <Text style={{ padding: 25, fontWeight: '800', fontSize: 36 }}>Good afternoon, Alex.</Text>
+          <View style={styles.content}>
+            <ListItem
+              scaleProps={{
+                friction: 90,
+                tension: 100,
+                activeScale: 0.95,
+              }}
+              linearGradientProps={{
+                colors: ['#10ac84', '#fefefe'],
+                start: [1, 0],
+                end: [0.2, 0],
+              }}
+              ViewComponent={LinearGradient}
+              leftIcon={{ name: 'redo', color: '#78e08f' }}
+              containerStyle={styles.listItem}
+              title="Login to get started!"
+            />
+            <ListItem
+              leftIcon={{ name: 'restaurant-menu', color: '#78e08f' }}
+              containerStyle={styles.listItem}
+              title="Check out the menu!"
+            />
+          </View>
+          <Header
+            backgroundColor={'#c6ffdd'}
+            outerContainerStyles={{ borderBottomWidth: 0, height: 140 }}
+            innerContainerStyles={{ alignItems: 'center' }}
+            centerComponent={{ text: 'SUPERGREEN', style: { color: '#f7797d' } }}
           />
         </View>
-        <Header
-          backgroundColor={'#c6ffdd'}
-          outerContainerStyles={{ borderBottomWidth: 0, height: 140 }}
-          innerContainerStyles={{ alignItems: 'center' }}
-          // leftComponent={{ icon: 'menu', color: '#f7797d' }}
-          centerComponent={{ text: 'SUPERGREEN', style: { color: '#f7797d' } }}
-          // rightComponent={{ icon: 'home', color: '#f7797d' }}
-        />
-      </View>
+      </React.Fragment>
     );
   }
 }
@@ -65,13 +78,17 @@ const styles = StyleSheet.create({
     flex: 1,
     height: '100%',
     width: '100%',
-    alignSelf: 'stretch',
     position: 'absolute',
-    justifyContent: 'center'
   },
   content: {
     flex: 1,
     justifyContent: 'flex-end',
-    // alignItems: 'center',
+  },
+  listItem: {
+    justifyContent: 'center',
+    backgroundColor: '#fefefe',
+    borderTopWidth: .5,
+    borderColor: '#999',
+    height: 75
   }
 });
